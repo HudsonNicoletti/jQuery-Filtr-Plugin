e0a38c2b042ffae1f95f7b8e1997b0ac8ee109d9
Hudson Nicoletti <hudson19962009@gmail.com>
2015-06-01 14:41:56 -0300

    V1.0
    New html

--- jQuery.filtr.js
+++ jQuery.filtr.js
@@ -11,48 +11,44 @@
             invert : false
         }, options );
         
-        if( settings.targetChild == null ) { 
-            var jo = $(settings.target) }
-        else {
-            var jo = $(settings.target).find(settings.targetChild);
-        }
+        var jo = ( settings.targetChild == null ) ? $(settings.target) : $(settings.target).find(settings.targetChild);
         
         $this.keyup(function () {
             
-                var data = this.value.split();
-            
-                if (this.value == "") {
-                    jo.show();
-                    return;
-                }
-            
-                if( settings.invert === true ){
-                    jo.show();
-                    jo.filter(function (i, v) {
-                        var $t = $(this);
-                        for (var d = 0; d < data.length; ++d) {
-                            if ($t.is(":contains('" + data[d] + "')")) {
-                                return true;
-                            }
+            var data = this.value.split();
+
+            if (this.value == "") {
+                jo.show();
+                return;
+            }
+
+            if( settings.invert === true ){
+                jo.show();
+                jo.filter(function (i, v) {
+                    var $t = $(this);
+                    for (var d = 0; d < data.length; ++d) {
+                        if ($t.is(":contains('" + data[d] + "')")) {
+                            return true;
                         }
-                        return false;
-                    }).hide();
-                }
-            
-                else{
-                    jo.hide();
-                    jo.filter(function (i, v) {
-                        var $t = $(this);
-                        for (var d = 0; d < data.length; ++d) {
-                            if ($t.is(":contains('" + data[d] + "')")) {
-                                return true;
-                            }
+                    }
+                    return false;
+                }).hide();
+            }
+
+            else{
+                jo.hide();
+                jo.filter(function (i, v) {
+                    var $t = $(this);
+                    for (var d = 0; d < data.length; ++d) {
+                        if ($t.is(":contains('" + data[d] + "')")) {
+                            return true;
                         }
-                        return false;
-                    }).show();
-                }
+                    }
+                    return false;
+                }).show();
+            }
             
-            });
+        });
         
         if( settings.caseSensitive === false ){
             jQuery.expr[':'].contains = function(a, i, m) {  return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0; };
